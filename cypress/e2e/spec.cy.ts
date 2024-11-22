69cff4e78160791283d1efb4d7094eba280490a5
thbrthbr <tarzenezrat@gmail.com>
2024-11-22 15:39:46 +0900

    projecct day1

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,26 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io');
+//   });
+// });
+describe('Homepage Tests', () => {
+  beforeEach(() => {
+    // 기본 경로로 이동
+    cy.visit('http://localhost:3002/');
+  });
+
+  it('should display "Get started by" text on the homepage', () => {
+    // "Get started by" 텍스트가 존재하는지 확인
+    cy.contains('Get started by').should('exist');
+  });
+
+  it('should display "Docs" anchor with correct href', () => {
+    cy.get('a', { timeout: 15000 }) // 15초 대기
+      .contains(/Docs/i)
+      .should(
+        'have.attr',
+        'href',
+        'https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app',
+      );
   });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+'use client';
+
+import Image from 'next/image';
+
 export default function Page() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -93,8 +97,7 @@ export default function Page() {
           />
           Go to nextjs.org →
         </a>
-      </footer> */}
-      Get started by editing
+      </footer>
     </div>
   );
 }
